16792f51ca64b81d5b385a0c78628615e0c79080
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-09-30 17:24:28 -0400

    Add function to create divs within the draw-area div

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const drawArea = document.querySelector('.draw-area');
+
+function createGrid() {
+    let size = 16;
+
+    for (let i = 0; i < (size * 2); i++){
+        const div = document.createElement('div');
+        drawArea.appendChild(div);
+    };
+};
+
+createGrid();
