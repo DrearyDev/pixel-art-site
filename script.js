471e8dd3478163d476fd1a47d0df5aa800897caa
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-09-30 17:47:07 -0400

    Fix error in math for amount of divs to create

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const drawArea = document.querySelector('.draw-area');
 function createGrid() {
     let size = 16;
 
-    for (let i = 0; i < (size * 2); i++){
+    for (let i = 0; i < (size ** 2); i++){
         const div = document.createElement('div');
         drawArea.appendChild(div);
     };
